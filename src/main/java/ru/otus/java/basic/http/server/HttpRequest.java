95a517bec889cefecccddd502bb9c366b75ef4d4
Nickolay <eninqwert@gmail.com>
2024-02-26 00:18:30 +0300

    hw31

--- src/main/java/ru/otus/java/basic/http/server/HttpRequest.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpRequest.java
@@ -1,5 +1,8 @@
 package ru.otus.java.basic.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +11,7 @@ public class HttpRequest {
     private String uri;
     private HttpMethod method;
     private Map<String, String> parameters;
+    public static final Logger logger = LogManager.getLogger(HttpRequest.class);
 
     public String getUri() {
         return uri;
